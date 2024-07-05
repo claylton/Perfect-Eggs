2d0bdb298c770d733e3b1ddd8666f5eb985c11a6
Claylton Santos <clayltonsp@hotmail.com>
2024-07-05 03:55:53 -0300

    ✨ feat: Create theme

---
+++ lib/themes/theme.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+const brightness = Brightness.dark;
+const seedColor =  Color(0xFFFDBF00);
+const secondaryColor =  Color(0xFFFFFFFF);
+
+ThemeData appTheme() {
+  return ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: seedColor,
+      brightness: brightness,
+      primaryContainer: secondaryColor,
+      onPrimaryContainer: Colors.black
+    ),
+
+    textTheme: const TextTheme(
+      bodyMedium: TextStyle(
+        fontFamily: "Montserrat",
+      ),
+      headlineMedium: TextStyle(
+        fontFamily: "Montserrat",
+      ),
+      titleLarge: TextStyle(
+        fontFamily: "Montserrat",
+      )
+    ),
+  );
+}
